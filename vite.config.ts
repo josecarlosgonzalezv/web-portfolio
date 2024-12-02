e9c6b98c7720bebc0ad57139e9b72e89f60cb801
José Carlos <86958774+josecarlosgonzalezv@users.noreply.github.com>
2024-12-02 20:13:25 +0100

    [PORTFOLIO] Update dependencies (#55)

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { checker } from 'vite-plugin-checker';
+// import { checker } from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    checker({
-      typescript: {
-        tsconfigPath: './tsconfig.json',
-      },
-      vueTsc: {
-        tsconfigPath: './tsconfig.json',
-      },
-    }),
+    // checker({
+    //   typescript: {
+    //     tsconfigPath: './tsconfig.json',
+    //   },
+    //   vueTsc: {
+    //     tsconfigPath: './tsconfig.json',
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
